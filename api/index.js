7dcb1bc5ca6974cb140afdc112a53eb1cb6e135c
be beet <63141491+beetcb@users.noreply.github.com>
2021-02-15 16:24:43 +0800

    feat: make base dir configrable

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const fetch = require('node-fetch')
 const { getToken, drive_api } = require('./api')
+const base_dir = process.env.base_dir
 
 async function getFile(path, access_token) {
   const res = await fetch(
-    `${drive_api}/root:${encodeURI(path)}?select=@microsoft.graph.downloadUrl`,
+    `${drive_api}/root:${encodeURI(
+      base_dir ? base_dir + path : path
+    )}?select=@microsoft.graph.downloadUrl`,
     {
       headers: {
         Authorization: `bearer ${access_token}`,
