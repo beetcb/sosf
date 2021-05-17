21d3de9c3d349d0d4d1184ac4ad129962f1bdfe7
beetcb <63141491+beetcb@users.noreply.github.com>
2021-05-17 11:52:08 +0800

    feat: render json to table

--- functions/sosf/index.js
+++ functions/sosf/index.js
@@ -1,6 +1,6 @@
 const { getToken, getItem, listChildren } = require('./api/sosf')
 
-async function handler({ path, queryStringParameters }) {
+async function handler({ path, queryStringParameters, headers }) {
   if (path === '/favicon.ico') {
     return null
   }
@@ -16,10 +16,42 @@ async function handler({ path, queryStringParameters }) {
         queryStringParameters.id
       )
       if (data) {
-        return data.value.reduce((obj, ele) => {
-          obj[ele.name] = ele.id
-          return obj
-        }, {})
+        const itemTable = data.value.reduce((arr, ele) => {
+          arr.push({ name: ele.name, id: ele.id })
+          return arr
+        }, [])
+        const docType = headers['content-type']
+        return {
+          isBase64Encoded: false,
+          statusCode: 200,
+          headers: {
+            'content-type': docType,
+          },
+          body: docType.includes('json')
+            ? itemTable
+            : `<!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <link
+                href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css"
+                rel="stylesheet"
+              />
+            </head>
+            <body>
+              <div id="wrapper"></div>
+              <script src="https://unpkg.com/gridjs/dist/gridjs.development.js"></script>
+              <script>
+              new gridjs.Grid({
+                columns: ['Name', 'ID'],
+                data: JSON.parse("${JSON.stringify(
+                  itemTable
+                )}")}).render(document.getElementById("wrapper"));
+              </script>
+            </body>
+          </html>
+          
+          `,
+        }
       }
     } else {
       const data = await getItem(path, access_token)
